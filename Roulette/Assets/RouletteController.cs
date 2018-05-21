4bbf9ce6621e354fe3e0520fa2bb641d77109d25
Unknown <g1827403@st.t-kougei.ac.jp>
2018-05-21 17:19:02 +0900

    komittt

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0;
+    //int MouseSta = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)/*||this.MouseSta==1*/)
+        {
+            this.rotSpeed = 20;
+           // this.MouseSta = 1;
+        }
+
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //if (Input.GetMouseButtonUp(0)||this.MouseSta==0)
+        //{
+            this.rotSpeed *= 0.97f;
+            //this.MouseSta = 0;
+        //}
+       
+	}
+}
